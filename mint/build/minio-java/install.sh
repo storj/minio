82cee1bd2a6f530955cd455ca090f8328c6f38cd
Bill Thorp <billt@storj.io>
2021-03-19 09:50:55 -0400

    minio: fix minio-Java version to 8.0.3 v2
    The following code used in testing was failing:
    docker build . -f Dockerfile.mint -t storj/mint
    
    This is because we hard-forked Minio, but it pulled in more recent
    version of minio-Java in its "Mint" tests.  So, we just hardcode the
    minio-Java to the last working version.
    
    The first fix for this I wasn't paying attention and I couldn't test
    because of an unrelated Docker issue.  This version does build locally.
    
    Change-Id: I09532f969ad23f3f09438ae00ec2432d6189a27a

--- mint/build/minio-java/install.sh
+++ mint/build/minio-java/install.sh
@@ -16,10 +16,11 @@
 #
 
 SPOTBUGS_VERSION="4.1.2" ## needed since 8.0.2 release
+MINIO_JAVA_VERSION="8.0.3"
 
 test_run_dir="$MINT_RUN_CORE_DIR/minio-java"
 git clone --quiet https://github.com/minio/minio-java.git "$test_run_dir/minio-java.git"
-(cd "$test_run_dir/minio-java.git"; git checkout --quiet "tags/8.0.3")
+(cd "$test_run_dir/minio-java.git"; git checkout --quiet "tags/${MINIO_JAVA_VERSION}")
 $WGET --output-document="$test_run_dir/minio-${MINIO_JAVA_VERSION}-all.jar" "https://repo1.maven.org/maven2/io/minio/minio/${MINIO_JAVA_VERSION}/minio-${MINIO_JAVA_VERSION}-all.jar"
 $WGET --output-document="$test_run_dir/spotbugs-annotations-${SPOTBUGS_VERSION}.jar" "https://repo1.maven.org/maven2/com/github/spotbugs/spotbugs-annotations/${SPOTBUGS_VERSION}/spotbugs-annotations-${SPOTBUGS_VERSION}.jar"
 javac -cp "$test_run_dir/minio-${MINIO_JAVA_VERSION}-all.jar:$test_run_dir/spotbugs-annotations-${SPOTBUGS_VERSION}.jar" "${test_run_dir}/minio-java.git/functional"/*.java
