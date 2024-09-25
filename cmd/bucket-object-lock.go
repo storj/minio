storj/minio

commit 00eca4dce0080d386464ed277797871a6255199b

cmd: remove global lock checks for CopyObjectHandler
CopyObjectHandler calls checkPutObjectLockAllowed which does some
checks of the request to see if retention is requested. However,
that function doesn't work as expected with our object lock feature
because it relies on globalBucketObjectLockSys in minio, but we
don't use that.

This change removes that functionality so the function is solely
used for validating retention parameters in the request only.

Updates storj/edge#499

Change-Id: I11b9d2e48bf9241a9482c7854dc340f4ee86291f

cmd/bucket-object-lock.go
@@ -23,12 +23,10 @@ import (
 	"os"
 	"strconv"
 
-	xhttp "storj.io/minio/cmd/http"
 	"storj.io/minio/cmd/logger"
 	"storj.io/minio/pkg/auth"
 	objectlock "storj.io/minio/pkg/bucket/object/lock"
 	"storj.io/minio/pkg/bucket/policy"
-	"storj.io/minio/pkg/bucket/replication"
 )
 
 // BucketObjectLockSys - map of bucket and retention configuration.
@@ -243,17 +241,9 @@ func enforceRetentionBypassForPut(ctx context.Context, r *http.Request, bucket, 
 	return oi, perm
 }
 
-// checkPutObjectLockAllowed enforces object retention policy and legal hold policy
-// for requests with WORM headers
+// parseObjectLockHeaders parses object retention and legal hold for requests.
 // See https://docs.aws.amazon.com/AmazonS3/latest/dev/object-lock-managing.html for the spec.
-// For non-existing objects with object retention headers set, this method returns ErrNone if bucket has
-// locking enabled and user has requisite permissions (s3:PutObjectRetention)
-// If object exists on object store and site wide WORM enabled - this method
-// returns an error. For objects in "Governance" mode, overwrite is allowed if the retention date has expired.
-// For objects in "Compliance" mode, retention date cannot be shortened, and mode cannot be altered.
-// For objects with legal hold header set, the s3:PutObjectLegalHold permission is expected to be set
-// Both legal hold and retention can be applied independently on an object
-func checkPutObjectLockAllowed(ctx context.Context, rq *http.Request, bucket, object string, getObjectInfoFn GetObjectInfoFn, retentionPermErr, legalHoldPermErr APIErrorCode) (objectlock.RetMode, objectlock.RetentionDate, objectlock.ObjectLegalHold, APIErrorCode) {
+func parseObjectLockHeaders(ctx context.Context, rq *http.Request, bucket, object string, retentionPermErr APIErrorCode) (objectlock.RetMode, objectlock.RetentionDate, objectlock.ObjectLegalHold, APIErrorCode) {
 	var mode objectlock.RetMode
 	var retainDate objectlock.RetentionDate
 	var legalHold objectlock.ObjectLegalHold
@@ -261,51 +251,8 @@ func checkPutObjectLockAllowed(ctx context.Context, rq *http.Request, bucket, ob
 	retentionRequested := objectlock.IsObjectLockRetentionRequested(rq.Header)
 	legalHoldRequested := objectlock.IsObjectLockLegalHoldRequested(rq.Header)
 
-	retentionCfg, err := globalBucketObjectLockSys.Get(bucket)
-	if err != nil {
-		return mode, retainDate, legalHold, ErrInvalidBucketObjectLockConfiguration
-	}
-
-	if !retentionCfg.LockEnabled {
-		if legalHoldRequested || retentionRequested {
-			return mode, retainDate, legalHold, ErrInvalidBucketObjectLockConfiguration
-		}
-
-		// If this not a WORM enabled bucket, we should return right here.
-		return mode, retainDate, legalHold, ErrNone
-	}
-
-	opts, err := getOpts(ctx, rq, bucket, object)
-	if err != nil {
-		return mode, retainDate, legalHold, toAPIErrorCode(ctx, err)
-	}
-
-	replica := rq.Header.Get(xhttp.AmzBucketReplicationStatus) == replication.Replica.String()
-
-	if opts.VersionID != "" && !replica {
-		if objInfo, err := getObjectInfoFn(ctx, bucket, object, opts); err == nil {
-			r := objectlock.GetObjectRetentionMeta(objInfo.UserDefined)
-			t, err := objectlock.UTCNowNTP()
-			if err != nil {
-				logger.LogIf(ctx, err)
-				return mode, retainDate, legalHold, ErrObjectLocked
-			}
-			if r.Mode == objectlock.RetCompliance && r.RetainUntilDate.After(t) {
-				return mode, retainDate, legalHold, ErrObjectLocked
-			}
-			mode = r.Mode
-			retainDate = r.RetainUntilDate
-			legalHold = objectlock.GetObjectLegalHoldMeta(objInfo.UserDefined)
-			// Disallow overwriting an object on legal hold
-			if legalHold.Status == objectlock.LegalHoldOn {
-				return mode, retainDate, legalHold, ErrObjectLocked
-			}
-		}
-	}
-
 	if legalHoldRequested {
-		var lerr error
-		if legalHold, lerr = objectlock.ParseObjectLockLegalHoldHeaders(rq.Header); lerr != nil {
+		if legalHold, err := objectlock.ParseObjectLockLegalHoldHeaders(rq.Header); err != nil {
 			return mode, retainDate, legalHold, toAPIErrorCode(ctx, err)
 		}
 	}
@@ -324,26 +271,7 @@ func checkPutObjectLockAllowed(ctx context.Context, rq *http.Request, bucket, ob
 		}
 		return rMode, rDate, legalHold, ErrNone
 	}
-	if replica { // replica inherits retention metadata only from source
-		return "", objectlock.RetentionDate{}, legalHold, ErrNone
-	}
-	if !retentionRequested && retentionCfg.Validity > 0 {
-		if retentionPermErr != ErrNone {
-			return mode, retainDate, legalHold, retentionPermErr
-		}
 
-		t, err := objectlock.UTCNowNTP()
-		if err != nil {
-			logger.LogIf(ctx, err)
-			return mode, retainDate, legalHold, ErrObjectLocked
-		}
-
-		if !legalHoldRequested && retentionCfg.LockEnabled {
-			// inherit retention from bucket configuration
-			return retentionCfg.Mode, objectlock.RetentionDate{Time: t.Add(retentionCfg.Validity)}, legalHold, ErrNone
-		}
-		return "", objectlock.RetentionDate{}, legalHold, ErrNone
-	}
 	return mode, retainDate, legalHold, ErrNone
 }
 

cmd/object-handlers.go
@@ -1196,14 +1196,9 @@ func (api ObjectAPIHandlers) CopyObjectHandler(w http.ResponseWriter, r *http.Re
 
 	srcInfo.UserDefined = objectlock.FilterObjectLockMetadata(srcInfo.UserDefined, true, true)
 	retPerms := isPutActionAllowed(ctx, getRequestAuthType(r), dstBucket, dstObject, r, iampolicy.PutObjectRetentionAction)
-	holdPerms := isPutActionAllowed(ctx, getRequestAuthType(r), dstBucket, dstObject, r, iampolicy.PutObjectLegalHoldAction)
-	getObjectInfo := objectAPI.GetObjectInfo
-	if api.CacheAPI() != nil {
-		getObjectInfo = api.CacheAPI().GetObjectInfo
-	}
 
 	// apply default bucket configuration/governance headers for dest side.
-	retentionMode, retentionDate, legalHold, s3Err := checkPutObjectLockAllowed(ctx, r, dstBucket, dstObject, getObjectInfo, retPerms, holdPerms)
+	retentionMode, retentionDate, legalHold, s3Err := parseObjectLockHeaders(ctx, r, dstBucket, dstObject, retPerms)
 	if s3Err == ErrNone && retentionMode.Valid() {
 		if dstOpts.Retention == nil {
 			dstOpts.Retention = &objectlock.ObjectRetention{}
@@ -1763,15 +1758,9 @@ func (api ObjectAPIHandlers) PutObjectExtractHandler(w http.ResponseWriter, r *h
 	}
 
 	retPerms := isPutActionAllowed(ctx, getRequestAuthType(r), bucket, object, r, iampolicy.PutObjectRetentionAction)
-	holdPerms := isPutActionAllowed(ctx, getRequestAuthType(r), bucket, object, r, iampolicy.PutObjectLegalHoldAction)
 
 	if api.CacheAPI() != nil {
 		putObject = api.CacheAPI().PutObject
-	}
-
-	getObjectInfo := objectAPI.GetObjectInfo
-	if api.CacheAPI() != nil {
-		getObjectInfo = api.CacheAPI().GetObjectInfo
 	}
 
 	putObjectTar := func(reader io.Reader, info os.FileInfo, object string) {
@@ -1816,7 +1805,7 @@ func (api ObjectAPIHandlers) PutObjectExtractHandler(w http.ResponseWriter, r *h
 		}
 		opts.MTime = info.ModTime()
 
-		retentionMode, retentionDate, legalHold, s3Err := checkPutObjectLockAllowed(ctx, r, bucket, object, getObjectInfo, retPerms, holdPerms)
+		retentionMode, retentionDate, legalHold, s3Err := parseObjectLockHeaders(ctx, r, bucket, object, retPerms)
 		if s3Err == ErrNone && retentionMode.Valid() {
 			metadata[strings.ToLower(xhttp.AmzObjectLockMode)] = string(retentionMode)
 			metadata[strings.ToLower(xhttp.AmzObjectLockRetainUntilDate)] = retentionDate.UTC().Format(iso8601TimeFormat)
@@ -2838,13 +2827,13 @@ func (api ObjectAPIHandlers) CompleteMultipartUploadHandler(w http.ResponseWrite
 	}
 
 	// Reject retention or governance headers if set, CompleteMultipartUpload spec
-	// does not use these headers, and should not be passed down to checkPutObjectLockAllowed
+	// does not use these headers, and should not be passed down to parseObjectLockHeaders
 	if objectlock.IsObjectLockRequested(r.Header) || objectlock.IsObjectLockGovernanceBypassSet(r.Header) {
 		WriteErrorResponse(ctx, w, errorCodes.ToAPIErr(ErrInvalidRequest), r.URL, guessIsBrowserReq(r))
 		return
 	}
 
-	if _, _, _, s3Err := checkPutObjectLockAllowed(ctx, r, bucket, object, objectAPI.GetObjectInfo, ErrNone, ErrNone); s3Err != ErrNone {
+	if _, _, _, s3Err := parseObjectLockHeaders(ctx, r, bucket, object, ErrNone); s3Err != ErrNone {
 		WriteErrorResponse(ctx, w, errorCodes.ToAPIErr(s3Err), r.URL, guessIsBrowserReq(r))
 		return
 	}

cmd/web-handlers.go
@@ -970,7 +970,6 @@ func (web *webAPIHandlers) Upload(w http.ResponseWriter, r *http.Request) {
 	}
 
 	retPerms := ErrAccessDenied
-	holdPerms := ErrAccessDenied
 	replPerms := ErrAccessDenied
 	if authErr != nil {
 		if authErr == errNoAuthToken {
@@ -1015,17 +1014,6 @@ func (web *webAPIHandlers) Upload(w http.ResponseWriter, r *http.Request) {
 			Claims:          claims.Map(),
 		}) {
 			retPerms = ErrNone
-		}
-		if GlobalIAMSys.IsAllowed(iampolicy.Args{
-			AccountName:     claims.AccessKey,
-			Action:          iampolicy.PutObjectLegalHoldAction,
-			BucketName:      bucket,
-			ConditionValues: getConditionValues(r, "", claims.AccessKey, claims.Map()),
-			IsOwner:         owner,
-			ObjectName:      object,
-			Claims:          claims.Map(),
-		}) {
-			holdPerms = ErrNone
 		}
 		if GlobalIAMSys.IsAllowed(iampolicy.Args{
 			AccountName:     claims.AccessKey,
@@ -1146,14 +1134,12 @@ func (web *webAPIHandlers) Upload(w http.ResponseWriter, r *http.Request) {
 	crypto.RemoveSensitiveEntries(metadata)
 
 	putObject := objectAPI.PutObject
-	getObjectInfo := objectAPI.GetObjectInfo
 	if web.CacheAPI() != nil {
 		putObject = web.CacheAPI().PutObject
-		getObjectInfo = web.CacheAPI().GetObjectInfo
 	}
 
 	// enforce object retention rules
-	retentionMode, retentionDate, _, s3Err := checkPutObjectLockAllowed(ctx, r, bucket, object, getObjectInfo, retPerms, holdPerms)
+	retentionMode, retentionDate, _, s3Err := parseObjectLockHeaders(ctx, r, bucket, object, retPerms)
 	if s3Err != ErrNone {
 		WriteErrorResponse(ctx, w, errorCodes.ToAPIErr(s3Err), r.URL, guessIsBrowserReq(r))
 		return

commit 18a5433825d62d271ad37b26ff90bc6e1d2bbc09

cmd: fix legal hold not passed through PutObject
this partially reverts fc1f7cd but sets retention and
legal hold options the same as other handlers like
CopyObjectHandler.

additionally, this change fixes parseObjectLockHeaders
to correctly set legalHold variable instead of throwing
it away if only legal hold was requested, but not legal
hold + retention. This was a regression introduced in
00eca4d.

Change-Id: I93b6d5bcdc79287b25636b1ca52fb65f18e9200f

cmd/bucket-object-lock.go
@@ -252,7 +252,8 @@ func parseObjectLockHeaders(ctx context.Context, rq *http.Request, bucket, objec
 	legalHoldRequested := objectlock.IsObjectLockLegalHoldRequested(rq.Header)
 
 	if legalHoldRequested {
-		if legalHold, err := objectlock.ParseObjectLockLegalHoldHeaders(rq.Header); err != nil {
+		var err error
+		if legalHold, err = objectlock.ParseObjectLockLegalHoldHeaders(rq.Header); err != nil {
 			return mode, retainDate, legalHold, toAPIErrorCode(ctx, err)
 		}
 	}

cmd/object-handlers.go
@@ -1506,19 +1506,25 @@ func (api ObjectAPIHandlers) PutObjectHandler(w http.ResponseWriter, r *http.Req
 		putObject = api.CacheAPI().PutObject
 	}
 
-	retentionRequested := objectlock.IsObjectLockRetentionRequested(r.Header)
+	retPerms := isPutActionAllowed(ctx, getRequestAuthType(r), bucket, object, r, iampolicy.PutObjectRetentionAction)
 
-	if retentionRequested {
-		retentionMode, retentionDate, err := objectlock.ParseObjectLockRetentionHeaders(r.Header)
-		if err != nil {
-			WriteErrorResponse(ctx, w, ToAPIError(ctx, err), r.URL, guessIsBrowserReq(r))
-			return
+	retentionMode, retentionDate, legalHold, s3Err := parseObjectLockHeaders(ctx, r, bucket, object, retPerms)
+	if s3Err == ErrNone {
+		if retentionMode.Valid() {
+			if opts.Retention == nil {
+				opts.Retention = &objectlock.ObjectRetention{}
+			}
+			opts.Retention.Mode = retentionMode
+			opts.Retention.RetainUntilDate = retentionDate
 		}
+		if legalHold.Status.Valid() {
+			opts.LegalHold = &legalHold.Status
+		}
+	}
 
-		opts.Retention = &objectlock.ObjectRetention{
-			Mode:            retentionMode,
-			RetainUntilDate: retentionDate,
-		}
+	if s3Err != ErrNone {
+		WriteErrorResponse(ctx, w, errorCodes.ToAPIErr(s3Err), r.URL, guessIsBrowserReq(r))
+		return
 	}
 
 	if ok, _ := mustReplicate(ctx, r, bucket, object, metadata, ""); ok {
@@ -1808,13 +1814,17 @@ func (api ObjectAPIHandlers) PutObjectExtractHandler(w http.ResponseWriter, r *h
 		opts.MTime = info.ModTime()
 
 		retentionMode, retentionDate, legalHold, s3Err := parseObjectLockHeaders(ctx, r, bucket, object, retPerms)
-		if s3Err == ErrNone && retentionMode.Valid() {
-			metadata[strings.ToLower(xhttp.AmzObjectLockMode)] = string(retentionMode)
-			metadata[strings.ToLower(xhttp.AmzObjectLockRetainUntilDate)] = retentionDate.UTC().Format(iso8601TimeFormat)
-		}
-
-		if s3Err == ErrNone && legalHold.Status.Valid() {
-			metadata[strings.ToLower(xhttp.AmzObjectLockLegalHold)] = string(legalHold.Status)
+		if s3Err == ErrNone {
+			if retentionMode.Valid() {
+				if opts.Retention == nil {
+					opts.Retention = &objectlock.ObjectRetention{}
+				}
+				opts.Retention.Mode = retentionMode
+				opts.Retention.RetainUntilDate = retentionDate
+			}
+			if legalHold.Status.Valid() {
+				opts.LegalHold = &legalHold.Status
+			}
 		}
 
 		if s3Err != ErrNone {